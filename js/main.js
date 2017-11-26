2e18f4c7fe5789e2a550dcabc3ec93c8ec476dce
Christina Paik <christinapaik@gmail.com>
2017-11-26 13:43:47 -0500

    add custom jquery to change font color of interests to white

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+// Textillate h1 animation
 $(function () {
     $('.tlt').textillate({ in: { effect: 'fadeIn' } });
 })
 
 // Backstrech as the body's background
 $.backstretch('img/ocean.jpg');
+
+
+// Custom jquery code
+$('h3').on( "click", function() {
+    $('h3').addClass('styled');
+});
